67146f3b99288ec175dccab10988b6fb689a02ca
Đạt <vovandat28051999@gmail.com>
2019-10-16 10:19:25 +0700

    done draw poly, working on check point inside poly

--- AStartAlgorithm.py
+++ AStartAlgorithm.py
@@ -77,10 +77,46 @@ class Node():
             res.append(node)
         return res
 
+class MyPoly():
+    def __init__(self,points):
+        self.points = points
+    
+    def getDrawPoints(self):
+        res = self.points.copy()
+        res.append(res[0])
+        return res
+    
+    def checkPointInSide(self,point):
+        isInside = False
+        minX = minY = math.inf
+        maxX = maxY = -math.inf
+        for p in self.points:
+            if minX > p[0]:
+                minX = p[0]
+            if maxX < p[0]:
+                maxX = p[0]
+            if minY > p[1]:
+                minY = p[1]
+            if maxY < p[1]:
+                maxY = p[1]
+        if point.x>=minX and point.x<=maxX and point.y>=minY and point.y<=maxY:
+            intersectCount = 0
+            if True:
+                return True
+        # print("minx: {} maxx: {}".format(minY,maxY))
+        return False
+
 class Matrix():
     def __init__(self, w, h):
         self.w = w
         self.h = h
+        self.polyInside = []
+
+    def addPolyInside(self,poly):
+        self.polyInside.append(poly)
+    
+    def getAllPolyInside(self):
+        return self.polyInside
 
 def aStar(matrix, start, end):
     nodeStart = Node(None,start)
@@ -95,6 +131,7 @@ def aStar(matrix, start, end):
     while len(openNodes)>0:
         #find the node in openNode having the lowest fScore[] value
         for node in openNodes:
+            # drawPoint(node.position.x,node.position.y,'cyan')
             if node.f <= currentNode.f:
                 currentNode = node
         pathResultNode.append(currentNode)
@@ -138,39 +175,47 @@ def getYInLine(p1,p2,x):
 def getXInLine(p1,p2,y):
     if p2.y-p1.y==0:
         return -1
-    return int(((p2.y-p1.y)*p1.x-(p2.x-p1.x)*y+(p2.x-p1.x)*p1.y)/(p2.y-p1.y))
+    return int(((p2.y-p1.y)*p1.x+(p2.x-p1.x)*y-(p2.x-p1.x)*p1.y)/(p2.y-p1.y))
 
-def drawPoly(pointPoly):
-    pointPoly.append(pointPoly[0])
+def drawPoly(poly):
+    pointPoly = poly.getDrawPoints()
+    for index in range(0,len(pointPoly)-1):
+        minX = pointPoly[index][0] if pointPoly[index][0] < pointPoly[index+1][0] else pointPoly[index+1][0]
+        maxX = pointPoly[index][0] if pointPoly[index][0] > pointPoly[index+1][0] else pointPoly[index+1][0]
+        for i in range(minX,maxX+1):
+            y = getYInLine(MyPoint(pointPoly[index][0],pointPoly[index][1]),MyPoint(pointPoly[index+1][0],pointPoly[index+1][1]),i)
+            if y!=-1:
+                drawPoint(i,y,'khaki')
+
+    for index in range(0,len(pointPoly)-1):
+        minY = pointPoly[index][1] if pointPoly[index][1] < pointPoly[index+1][1] else pointPoly[index+1][1]
+        maxY = pointPoly[index][1] if pointPoly[index][1] > pointPoly[index+1][1] else pointPoly[index+1][1]
+        for i in range(minY,maxY+1):
+            x = getXInLine(MyPoint(pointPoly[index][0],pointPoly[index][1]),MyPoint(pointPoly[index+1][0],pointPoly[index+1][1]),i)
+            if x!=-1:
+                drawPoint(x,i,'khaki')
     for point in pointPoly:
-        drawPoint(point[0],point[1],'red')
-        for index in range(0,len(pointPoly)-1):
-            minX = pointPoly[index][0] if pointPoly[index][0] < pointPoly[index+1][0] else pointPoly[index+1][0]
-            maxX = pointPoly[index][0] if pointPoly[index][0] > pointPoly[index+1][0] else pointPoly[index+1][0]
-            for i in range(minX,maxX+1):
-                y = getYInLine(MyPoint(pointPoly[index][0],pointPoly[index][1]),MyPoint(pointPoly[index+1][0],pointPoly[index+1][1]),i)
-                if y!=-1:
-                    drawPoint(i,y,'red')
-
-            # minY = pointPoly[index][1] if pointPoly[index][1] < pointPoly[index+1][1] else pointPoly[index+1][1]
-            # maxY = pointPoly[index][1] if pointPoly[index][1] > pointPoly[index+1][1] else pointPoly[index+1][1]
-            # for i in range(minY,maxY+1):
-            #     x = getXInLine(MyPoint(pointPoly[index][0],pointPoly[index][1]),MyPoint(pointPoly[index+1][0],pointPoly[index+1][1]),i)
-            #     if x!=-1:
-            #         drawPoint(x,i,'blue')
+        drawPoint(point[0],point[1],'darkkhaki')
 
 def main():
-    pointStart = MyPoint(1,5)
-    pointEnd = MyPoint(96,79)
-    pickupPoint = [(15,20),(20,30),(25,60),(35,50),(45,79)]
-    matrix = Matrix(100,100)
+    pointStart = MyPoint(2,2)
+    pointEnd = MyPoint(19,16)
+    # pickupPoint = [(15,20),(20,30),(25,60),(35,50),(45,79)]
+    pickupPoint = []
+    matrix = Matrix(22,22)
     drawFirstWin(matrix)
 
-    #draw poly
-    # drawPoly([(8,12),(8,17),(13,12)])
-    # drawPoly([(4,4),(5,9),(8,10),(9,5)])
-    # drawPoly([(11,1),(11,6),(14,6),(14,1)])
-    
+    # draw poly
+    poly1 = MyPoly([(8,12),(8,17),(13,12)])
+    poly2 = MyPoly([(4,4),(5,9),(8,10),(9,5)])
+    poly3 = MyPoly([(11,1),(11,6),(14,6),(14,1)])
+    poly1.checkPointInSide(MyPoint())
+    drawPoly(poly1)
+    drawPoly(poly2)
+    drawPoly(poly3)
+    matrix.addPolyInside(poly1)
+    matrix.addPolyInside(poly2)
+    matrix.addPolyInside(poly3)
 
     for point in pickupPoint:
         drawPoint(point[0],point[1],'red')
